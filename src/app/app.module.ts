e441527d5823434c1bfff7a15ef1c53113116290
VigneshCBE <vignesh.ganesan.cbe@outlook.com>
2017-07-11 09:45:06 +0530

    Revert "get by id"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,10 @@ import { HttpModule } from '@angular/http';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService }  from './webapi.service';
-
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
-
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -27,8 +23,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     HttpModule,
-    IonicModule.forRoot(MyApp),
-    InMemoryWebApiModule.forRoot(InMemoryDataService)
+    IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/contact/contact.service.ts
+++ src/pages/contact/contact.service.ts
@@ -1,54 +1,31 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
-
-import { InMemoryDataService } from './../../app/webapi.service';
-
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/toPromise';
-
 
 import { IContacts } from './contactListInterface';
 
 @Injectable()
 export class ContactService {
 
-//    private _contactsUrl = "http://demo0790365.mockable.io/employee/employeeList";
-//      private _contactsUrl = "./../../assets/api/employeeList.json";
-private _getContactsUrl = "api/EmployeeList";
-
+   private _contactsUrl = "http://demo0790365.mockable.io/employee/employeeList";
 
    constructor(private _http : Http) {
 
    }
 
-    // getContactList(): Observable<IContacts[]> {
-    //     return this._http.get(this._contactsUrl)
-    //             .map((response : Response) => <IContacts[]> response.json())
-    //             .do(data => console.log('All: ' + JSON.stringify(data)))
-    //             .catch(this.handleError);
-    // }
-
-    getThissContact(empid : string) : Promise<IContacts[]> {
-        const url = this._getContactsUrl +"/empid="+empid;
-        return this._http.get(this._getContactsUrl)
-                        .toPromise()
-                        .then(response => response.json().data as IContacts[])
-                        .catch(this.handleError);
+    getContactList(): Observable<IContacts[]> {
+        return this._http.get(this._contactsUrl)
+                .map((response : Response) => <IContacts[]> response.json())
+                .do(data => console.log('All: ' + JSON.stringify(data)))
+                .catch(this.handleError);
     }
 
-    getContactList() : Promise<IContacts[]> {
-        return this._http.get(this._getContactsUrl)
-                            .toPromise()
-                            .then(response => response.json().data as IContacts[])
-                            .catch(this.handleError);
-    }
-
-    private handleError(error : any) : Promise<any> {
+    private handleError(error : Response) {
         console.error(error);
-        return Promise.reject(error.json().error || 'server error');
+        return Observable.throw(error.json().error || 'server error');
     }
 
 }
